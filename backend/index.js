623080ed6a6190f7e9c7449a092e921715dbd6c3
muditha <muditha.kumara@centria.fi>
2025-04-22 22:28:31 +0300

    create backend and frrontend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+// filepath: /home/muditha/AdvanceWeb/AWD_FinalProject/backend/index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
