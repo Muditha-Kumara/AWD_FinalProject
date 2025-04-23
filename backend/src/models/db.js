207ca9bafe7b8de7f75bfa6e4af6980515277a3a
muditha <muditha.kumara@centria.fi>
2025-04-24 02:51:30 +0300

    Clean up debug logs and finalize database connection setup

--- backend/src/models/db.js
+++ backend/src/models/db.js
@@ -7,12 +7,5 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
-console.log({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
 
 module.exports = pool;
